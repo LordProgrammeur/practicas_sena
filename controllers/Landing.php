eab0feef1ae3649c08887af339fc0a7a27ab29ce
LordProgrammeur <david91ortiz14@gmail.com>
2024-05-07 21:43:55 -0500

    creacion de objeto y fuciones

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -1,3 +1,9 @@
 <?php
-    echo "estoy en el controlador landing";
+    class Landing{ //las clases tienen apertura y cierre y fucnion constructora
+        //public function __construct(){}// construct es una funcion que permite construir el objeto con sus cracteristicas
+        public function main(){
+            require_once "views/company/index.view.php";
+        }
+    }
+    require_once "views/company/index.view.php"; //el landing carga la vista de index.view.php
 ?>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <!-- index siempre llama controladores -->
 <?php
-    require_once "controllers/Landing.php" //aca se llama como una pegatina a landing y lo pega al frente
+    require_once "controllers/Landing.php"; //aca se llama como una pegatina a landing y lo pega al frente
+    $controller = new Landing;// si no se crea el objeto no se puede usar la funcion
+    $controller->main();//el main corre la fiucnion creada en landing
 ?>
